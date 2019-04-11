d14b520536c95c97ace3e75d4c92e796f95a0e89
Artik <artik.alpha@gmail.com>
2019-04-11 23:09:36 +0300

    UPD: layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'hiddens';
+  users = [
+    { name: 'Petya', last_message: Date.now() },
+    { name: 'Vasya', last_message: Date.now() - 1000000 },
+  ];
 }
