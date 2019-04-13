4cd9a39f9cc76b1eba162097fb9ff9a407955c54
Artik <artik.alpha@gmail.com>
2019-04-13 12:07:35 +0300

    ADD: angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 import { WebSocketPostService } from './services/ws';
 import { StateService } from './services/state';
@@ -20,8 +22,12 @@ import { ChatComponent } from './components/chat';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // Material
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [
     WebSocketPostService,
